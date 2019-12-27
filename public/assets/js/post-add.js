08748b3549049c323bba4bab9a44f235fa1d9fcf
Xavier <383792342@qq.com>
2019-12-27 18:33:04 +0800

    文章页面图片上传功能

--- public/assets/js/post-add.js
+++ public/assets/js/post-add.js
@@ -3,9 +3,31 @@ $.ajax({
     url:'/categories',
     type:'get',
     success: function(response) {
-        console.log(response)
+        // console.log(response)
         var html = template('categorTpl', {data:response})
         $('#category').html(html)
 
     }
+})
+//实现图片文件上传
+$('#feature').on('change', function() {
+    // 获取选择到的文件
+    var files = this.files[0];
+    // console.log(files)
+    // 创建formdata使图片变成二进制格式
+    var formData = new FormData();
+    // 将管理员选择的文件添加到formdata中
+    formData.append('cover',files )
+    $.ajax({
+        url:'/upload',
+        type:'post',
+        data:formData,
+        processData:false,
+        contentType:false,
+        success: function(response) {
+            // console.log(response)
+            $('#thumbnail').val(response[0].cover)
+        }
+    
+    })
 })
