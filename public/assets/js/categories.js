4718a662493bb0cde8bf9a22fe70ca8bf6fdd35a
Xavier <383792342@qq.com>
2019-12-27 10:09:49 +0800

    分类数据展示

--- public/assets/js/categories.js
+++ public/assets/js/categories.js
@@ -14,3 +14,13 @@ $('#addCategories').on('submit', function() {
     // 阻止表单默认提交行为
     return false;
 })
+//分类数据展示在页面中
+$.ajax({
+    url:'/categories',
+    type:'get',
+    success: function(response) {
+        // console.log(response)
+        var html = template('categoriesTpl',{data:response})
+        $('#categoriesBody').html(html)
+    }
+})
